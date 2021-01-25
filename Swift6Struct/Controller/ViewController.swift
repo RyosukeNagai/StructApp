b0c81b50ce203d36644565374ad31bc3ad34cdbe
vagrant <vagrant@localhost.localdomain>
2021-01-25 12:20:12 +0000

    Cellの作成

--- Swift6Struct/Controller/ViewController.swift
+++ Swift6Struct/Controller/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource  {
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier:"Cell", for: indexPath)
+        
+        let userNameLabel = cell.contentView.viewWithTag(1) as! UILabel
+        
+        let shumiLabel = cell.contentView.viewWithTag(2) as! UILabel
+        
+        let movieLabel = cell.contentView.viewWithTag(3) as! UILabel
+        
+        return cell
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    
+    
 }
 
