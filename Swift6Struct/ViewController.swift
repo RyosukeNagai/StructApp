cf4a000bf53da591ce72327aad4aec8dda82f327
vagrant <vagrant@localhost.localdomain>
2021-01-25 10:39:59 +0000

    first commit

---
+++ Swift6Struct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6Struct
+//
+//  Created by 長井崚介 on 2021/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
